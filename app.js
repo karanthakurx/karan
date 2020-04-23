113170fa50783a1370f4dcdf606723e28b5f0909
karanthakurx <57019391+karanthakurx@users.noreply.github.com>
2020-04-23 12:33:47 -0700

    ya

---
+++ app.js
@@ -0,0 +1,11 @@
+let element =document.getElementById("enter");
+let can =document.getElementById("can");
+
+
+
+function karle(params) {
+    element.innerHTML="i dit it"
+}
+function karle1(params) {
+    can.innerHTML="youit"
+}
